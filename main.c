4c589e731852d12e537eff6db8dd713235a7dda6
terrantsh <tanshanhe@foxmail.com>
2018-09-07 15:50:35 +0800

    [use api port]

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "sha256.h"
+#include <windows.h>
+
+void  wc_Sha256GetHash(wc_Sha256* sha256, byte* hash);
 
 int main() {
-    printf("Hello, World!\n");
+    char text [] = "78000100";
+    wc_Sha256GetHash(wc_Sha256* text, byte* text);
+    puts();
     return 0;
 }
